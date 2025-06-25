7b4daa1ef37bc9f9a265b04ddc511679240760cb
DarkFeed2005 <kalanayasassrisathruwan123@gmail.com>
2025-06-25 22:23:16 +0530

    LoadingSpinners.jsx

---
+++ animated/src/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="flex items-center justify-center h-screen w-full bg-black">
+     Spin@1x-1.0s-200px-200px.svg
+    </div>
+  );
+};
+export default LoadingSpinner;
